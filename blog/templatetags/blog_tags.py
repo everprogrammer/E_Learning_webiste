20ff9da09fe9873176e65a7936c699f763bbaebb
everprogrammer <amir.tavakolian68@gmail.com>
2023-08-30 17:57:46 +0330

    Edit html files
    - Edit html files for dynamic response, template tags

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,40 @@
+from django import template
+from blog.models import Post, Category
+
+register = template.Library()
+
+@register.simple_tag(name='totalposts')
+def total_posts():
+    return Post.objects.filter(published_status=1).count()
+
+@register.simple_tag(name='published_posts')
+def published_posts():
+    posts = Post.objects.filter(published_status=1).order_by('-published_date')[:3]
+    return posts
+
+
+@register.inclusion_tag('blog/blog-sidebar.html')
+def recent_posts():
+    # Recent posts
+    posts = Post.objects.filter(published_status=1).order_by('-published_date')[:3]
+    return {'myposts': posts}
+
+# @register.inclusion_tag('blog/blog-category.html')
+# def postcategories():
+#     posts = Post.objects.filter(published_status=1)
+#     categories = Category.objects.all()
+#     cat_dict = {}
+#     for name in categories:
+#         cat_dict[name] = posts.filter(category=name).count()
+
+#     return {'categories': cat_dict}
+
+@register.simple_tag()
+def postcategories():
+    posts = Post.objects.filter(published_status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+
+    return {'categories': cat_dict}
