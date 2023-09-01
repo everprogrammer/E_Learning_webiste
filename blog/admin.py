d761607db4f1ade904b621ee10a8653ebd13ec83
everprogrammer <amir.tavakolian68@gmail.com>
2023-09-01 18:25:50 +0330

    Create Comment model
    - add Comment class
    - register comment in admin.py
    - customize comment admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category 
+from .models import Post, Category, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -11,4 +11,14 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('published_status', 'published_date', 'author')
     summernote_fields = ('content',)
 
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name', 'post', 'is_approved', 'created_date')
+    list_filter = ('post', 'is_approved',)
+    search_fields = ['name', 'post']
+
+
+
 admin.site.register(Category)
+admin.site.register(Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -41,4 +41,19 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('blog:blog_single', kwargs={'pid': self.id})
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    comment = models.TextField()
+    is_approved = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if len(self.comment) >= 10:
+            return self.comment[:10]
+        return self.comment
 
