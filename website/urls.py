8e5fa05f7781998f1d73b8e41857f5976e55baa2
everprogrammer <amir.tavakolian68@gmail.com>
2023-09-03 17:57:44 +0330

    fix bug localhost:8000/contact

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,7 @@ app_name = 'website'
 
 urlpatterns = [
     path('', views.index_view, name='index_view'),
-    path('contact', views.contact_view, name='contact_view'),
+    path('contact/', views.contact_view, name='contact_view'),
     path('test', views.test_view, name='test_view'),
-    path('newsletter', views.newsletter_view, name='newsletter_view'),
+    path('newsletter/', views.newsletter_view, name='newsletter_view'),
 ]   
